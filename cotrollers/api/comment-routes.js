7d9b7accf029b2635181d7a0e2af6ec7535befd6
krupali pilgulwar <rpkrupali@gmail.com>
2022-03-07 17:43:51 -0600

    Comment routes to get all comments, post a comment and delete comment

--- cotrollers/api/comment-routes.js
+++ cotrollers/api/comment-routes.js
@@ -1,16 +1,42 @@
-const router = require('express').Router();
-const { Comment } = require('../../models');
+const router = require("express").Router();
+const { Comment, Post, User } = require("../../models");
 
-router.get('/',(req,res)=>{
-
+router.get("/", (req, res) => {
+  Comment.findAll()
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => res.status(500).json(err));
 });
 
-router.post('/', (req, res) => {
-
+router.post("/", (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.body.user_id,
+    post_id: req.body.post_id,
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ err: "Error creating comment" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete("/:id", (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "No record found with given id" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
