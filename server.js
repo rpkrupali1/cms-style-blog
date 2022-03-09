8b45de29717b9c5303e10d5dceede192f186d7f8
krupali pilgulwar <rpkrupali@gmail.com>
2022-03-08 19:34:47 -0600

    added partials and utils

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const path = require("path");
+const helpers = require("./utils/helpers");
 const express = require("express");
 const routes = require("./cotrollers/");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 const session = require("express-session");
 const SequalizeStore = require("connect-session-sequelize")(session.Store);
 
