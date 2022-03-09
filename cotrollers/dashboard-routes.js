b743b2ae008a95b5845f9645e926e9fe3c48fa8c
krupali pilgulwar <rpkrupali@gmail.com>
2022-03-09 17:34:27 -0600

    user can edit or delete posts

--- cotrollers/dashboard-routes.js
+++ cotrollers/dashboard-routes.js
@@ -3,7 +3,7 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
-router.get("/", withAuth,(req, res) => {
+router.get("/", withAuth, (req, res) => {
   // find all posts created by that user
   Post.findAll({
     where: {
@@ -34,9 +34,40 @@ router.get("/", withAuth,(req, res) => {
       });
     })
     .catch((err) => {
-      console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+        res.render("edit-post", {
+          post,
+          loggedIn: true,
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
