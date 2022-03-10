222cb4234e4bebfabdc49ea12b4f32083a4b0219
krupali pilgulwar <rpkrupali@gmail.com>
2022-03-10 06:00:56 -0600

    Create Post is now added to modal

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -16,6 +16,62 @@ async function newFormHandler(event) {
   if (response.ok) document.location.replace("/dashboard");
   else alert(response.statusText);
 }
-document
-  .querySelector(".new-post-form")
-  .addEventListener("submit", newFormHandler);
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add("is-active");
+  }
+
+  function closeModal($el) {
+    $el.classList.remove("is-active");
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll(".modal") || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll(".js-modal-trigger") || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+    console.log($target);
+
+    $trigger.addEventListener("click", () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (
+    document.querySelectorAll(
+      ".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button"
+    ) || []
+  ).forEach(($close) => {
+    const $target = $close.closest(".modal");
+
+    $close.addEventListener("click", () => {
+      closeModal($target);
+    });
+  });
+
+  document
+    .querySelector(".new-post-form")
+    .addEventListener("submit", newFormHandler);
+
+  // Add a keyboard event to close all modals
+  document.addEventListener("keydown", (event) => {
+    const e = event || window.event;
+
+    if (e.keyCode === 27) {
+      // Escape key
+      closeAllModals();
+    }
+  });
+});
+
+// document
+//   .querySelector(".new-post-form")
+//   .addEventListener("submit", newFormHandler);
